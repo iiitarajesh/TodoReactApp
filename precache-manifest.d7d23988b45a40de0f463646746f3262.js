72c0a480c492cf965b483577ca927ad6764825f3
Kumar <703225299@genpact.com>
2020-07-04 18:07:21 +0530

    Updates

---
+++ precache-manifest.d7d23988b45a40de0f463646746f3262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3beb7859142be2cc0fa04a5a3ccef7a",
+    "url": "/iiitarajesh/TodoReactApp/index.html"
+  },
+  {
+    "revision": "56e9f8d6b3f44be90024",
+    "url": "/iiitarajesh/TodoReactApp/static/css/main.68b7aad5.chunk.css"
+  },
+  {
+    "revision": "65bdef71168010873689",
+    "url": "/iiitarajesh/TodoReactApp/static/js/2.90913f65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iiitarajesh/TodoReactApp/static/js/2.90913f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e9f8d6b3f44be90024",
+    "url": "/iiitarajesh/TodoReactApp/static/js/main.00e5f792.chunk.js"
+  },
+  {
+    "revision": "c0678766fb0ea6a17c0b",
+    "url": "/iiitarajesh/TodoReactApp/static/js/runtime-main.c84880df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iiitarajesh/TodoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
